e21e081b40795f5ff3a7a1b871e639b8c3b0fc8b
Maksim_Huretski <6115003@gmail.com>
2018-08-15 15:49:34 +0300

    front input added

--- java/maksim_huretski/myapplication/MainActivity.java
+++ java/maksim_huretski/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package maksim_huretski.myapplication;
 
+import android.graphics.drawable.GradientDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        possibleValues = findViewById(R.id.possibleValues);
+        possibleValues.setVisibility(View.GONE);
+    }
+
+    private TextView cell;
+    private View possibleValues;
+    private GradientDrawable drawable;
+    private int[][] sudoku = new int[9][9];
+
+    public void onClickCell(View view) {
+        if (drawable != null) drawable.setStroke(3, getResources().getColor(R.color.border));
+        cell = findViewById(view.getId());
+        possibleValues.setVisibility(View.VISIBLE);
+        drawable = (GradientDrawable)view.getBackground();
+        drawable.setStroke(10, getResources().getColor(R.color.colorAccent));
+    }
+
+    public void onClickValue(View view) {
+        TextView value = findViewById(view.getId());
+        String cellText = String.valueOf(value.getText());
+        if (!cellText.equals("0")) cell.setText(cellText);
+        else cell.setText(R.string.vDefault);
+        drawable.setStroke(3, getResources().getColor(R.color.border));
+        possibleValues.setVisibility(View.GONE);
+    }
+
+    public void onClickCalculate(View view) {
     }
 }
